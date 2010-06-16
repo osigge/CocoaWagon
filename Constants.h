16dec433aa39fd92f42f7f1b132385094f88f90c
Yves Vogl <yves.vogl@mac.com>
2010-06-16 17:34:45 +0200

    Added Constants, e.g. for multipart/form-data boundaries.

---
+++ Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  CocoaWagon
+//
+//  Created by Yves Vogl on 15.06.10.
+//  Copyright 2010 DEETUNE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+extern NSString * const MULTIPART_FORM_DATA_BOUNDARY;
+
+@interface Constants : NSObject {}
+
+@end
---
+++ Constants.m
@@ -0,0 +1,16 @@
+//
+//  Constants.m
+//  CocoaWagon
+//
+//  Created by Yves Vogl on 15.06.10.
+//  Copyright 2010 DEETUNE. All rights reserved.
+//
+
+#import "Constants.h"
+
+
+@implementation Constants
+
+NSString * const MULTIPART_FORM_DATA_BOUNDARY = @"F20FDFA9A4C92F31BB7F4B86A3D08D300463220439AD6B6B6C022E44E190F2CB";
+
+@end
